ca661f08a8101465cbfbe59f21204924373e2d0f
Brandon Foster <brandon.foster.vt@gmail.com>
2023-09-03 17:40:26 -0400

    mt-2 on copy confirmation

--- server/client/src/comp/CopyConfirmation.js
+++ server/client/src/comp/CopyConfirmation.js
@@ -1,6 +1,6 @@
 export const CopyConfirmation = () => {
     return (
-        <div className='modal-body'>
+        <div className='modal-body mt-2'>
             <p><i className='bi bi-clipboard-check'></i> Copied to clipboard</p>
         </div>
     );
